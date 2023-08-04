80584f46ccca8910508d063a7b26eda1299550d4
delisma <david.elisma@tbs-sct.gc.ca>
2023-08-03 22:09:16 -0400

    aider: Remove duplicate import statements for `pypdfium2` and `BytesIO` in `new_file/pdf_to_webhook.py`.
    USER: I did `git reset --hard HEAD~1` to discard the last edits.
    
    ASSISTANT: Alright. If you need any further assistance with your code, feel free to ask.
    
    USER: Are line 21 and 22 duplicate of line 7 and 8?
    
    ASSISTANT: Yes, you're correct. The lines 21 and 22 in `new_file/pdf_to_webhook.py` are indeed duplicates of lines 7 and 8. They import the same modules (`pypdfium2 as pdfium` and `from io import BytesIO`) and should be removed to avoid redundancy and potential confusion. Here's how you can update the file:
    
    ```python
    new_file/pdf_to_webhook.py
    <<<<<<< ORIGINAL
    import pypdfium2 as pdfium
    from io import BytesIO
    =======
    >>>>>>> UPDATED
    ```

--- new_file/pdf_to_webhook.py
+++ new_file/pdf_to_webhook.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAi
+from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
@@ -6,8 +6,6 @@ from pytesseract import image_to_string
 from PIL import Image
 from io import BytesIO
 import pypdfium2 as pdfium
-from urllib.parse import urlparse
-from requests import get
 import streamlit as st
 import multiprocessing
 from tempfile import NamedTemporaryFile
@@ -15,84 +13,150 @@ import pandas as pd
 import json
 import requests
 
-# Load environment variables from .env file
 load_dotenv()
 
-import pypdfium2 as pdfium
-from io import BytesIO
-
-# 1 Convert a PDF file into images using PyPDFium2.
-def convert_pdf_to_images(pdf_path, scale=300/72):
-    # Load the PDF
-    pdf = pdfium.PdfDocument(pdf_path)
-    
-    # Convert each page to an image
-    images = []
-    for page in range(pdf.page_count):
-        bitmap = pdf.render_page(page, scale=scale)
-        image = Image.frombuffer('RGBA', (bitmap.width, bitmap.height), bitmap.buffer, 'raw', 'RGBA', 0, 1)
-        
-        # Save the image to a BytesIO object
-        image_data = BytesIO()
-        image.save(image_data, format='PNG')
-        image_data.seek(0)
-        
-        images.append(image_data)
-    
-from pytesseract import image_to_string
-
-# 2 Extract text from the images using PyTesseract.
-def extract_text_from_image(images):
-    # Initialize an empty list to store the text from each image
-    texts = []
-    
-    # Iterate over the list of images
-    for image in images:
-        # Convert the image to text and append it to the list
-        texts.append(pytesseract.image_to_string(image))
-    
-    # Join the list of texts into a single string and return it
-    return ' '.join(texts)
-
-# 3 Extract structured information from the text using the Langchain library to access the OpenAI API.
-# def extract_info_from_text(text):
-    # TODO: Extract structured information from the text
+# 1. Convert PDF file into images via pypdfium2
 
 
-# 4 Send the extracted data to make.com via a webhook.
-# def send_data_to_webhook(data):
-    # TODO: Send the data to make.com via a webhook
-# No changes here
+def convert_pdf_to_images(file_path, scale=300/72):
 
-def extract_content_from_url(url_or_path):
-    # Check if the input is a URL or a local file path
-    parsed = urlparse(url_or_path)
-    if parsed.scheme in ('http', 'https'):
-        # It's a URL, download the file
-        response = get(url_or_path)
-        with NamedTemporaryFile(suffix=".pdf", delete=False) as temp_pdf:
-            temp_pdf.write(response.content)
-        file_path = temp_pdf.name
-    else:
-        # It's a local file path, no need to download
-        file_path = url_or_path
+    pdf_file = pdfium.PdfDocument(file_path)
 
-    # Convert the PDF into images
-    images = convert_pdf_to_images(file_path)
-    
-    # Extract the text from the images
-    text = extract_text_from_image(images)
-    
-    # Return the extracted text
-    return text
+    page_indices = [i for i in range(len(pdf_file))]
 
-def main():
-    # TODO: Main function to tie all the steps together
-    content = extract_content_from_url(
-        "DE20220228.pdf"
+    renderer = pdf_file.render(
+        pdfium.PdfBitmap.to_pil,
+        page_indices=page_indices,
+        scale=scale,
     )
-    print(content)
 
-if __name__ == "__main__":
+    final_images = []
+
+    for i, image in zip(page_indices, renderer):
+
+        image_byte_array = BytesIO()
+        image.save(image_byte_array, format='jpeg', optimize=True)
+        image_byte_array = image_byte_array.getvalue()
+        final_images.append(dict({i: image_byte_array}))
+
+    return final_images
+
+# 2. Extract text from images via pytesseract
+
+
+def extract_text_from_img(list_dict_final_images):
+
+    image_list = [list(data.values())[0] for data in list_dict_final_images]
+    image_content = []
+
+    for index, image_bytes in enumerate(image_list):
+
+        image = Image.open(BytesIO(image_bytes))
+        raw_text = str(image_to_string(image))
+        image_content.append(raw_text)
+
+    return "\n".join(image_content)
+
+
+def extract_content_from_url(url: str):
+    images_list = convert_pdf_to_images(url)
+    text_with_pytesseract = extract_text_from_img(images_list)
+
+    return text_with_pytesseract
+
+
+# 3. Extract structured info from text via LLM
+def extract_structured_data(content: str, data_points):
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+    template = """
+    You are an expert admin people who will extract core information from documents
+
+    {content}
+
+    Above is the content; please try to extract all data points from the content above 
+    and export in a JSON array format:
+    {data_points}
+
+    Now please extract details from the content  and export in a JSON array format, 
+    return ONLY the JSON array:
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["content", "data_points"],
+        template=template,
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    results = chain.run(content=content, data_points=data_points)
+
+    return results
+
+
+# 4. Send data to make.com via webhook
+def send_to_make(data):
+    # Replace with your own link
+    webhook_url = "https://hook.eu1.make.com/xxxxxxxxxxxxxxxxx"
+
+    json = {
+        "data": data
+    }
+
+    try:
+        response = requests.post(webhook_url, json=json)
+        response.raise_for_status()  # Check for any HTTP errors
+        print("Data sent successfully!")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send data: {e}")
+
+
+# 5. Streamlit app
+def main():
+    default_data_points = """{
+        "invoice_item": "what is the item that charged",
+        "Amount": "how much does the invoice item cost in total",
+        "Company_name": "company that issued the invoice",
+        "invoice_date": "when was the invoice issued",
+    }"""
+
+    st.set_page_config(page_title="Doc extraction", page_icon=":bird:")
+
+    st.header("Doc extraction :bird:")
+
+    data_points = st.text_area(
+        "Data points", value=default_data_points, height=170)
+
+    uploaded_files = st.file_uploader(
+        "upload PDFs", accept_multiple_files=True)
+
+    if uploaded_files is not None and data_points is not None:
+        results = []
+        for file in uploaded_files:
+            with NamedTemporaryFile(dir='.', suffix='.csv') as f:
+                f.write(file.getbuffer())
+                content = extract_content_from_url(f.name)
+                print(content)
+                data = extract_structured_data(content, data_points)
+                json_data = json.loads(data)
+                if isinstance(json_data, list):
+                    results.extend(json_data)  # Use extend() for lists
+                else:
+                    results.append(json_data)  # Wrap the dict in a list
+
+        if len(results) > 0:
+            try:
+                df = pd.DataFrame(results)
+                st.subheader("Results")
+                st.data_editor(df)
+                if st.button("Sync to Make"):
+                    send_to_make(results)
+                    st.write("Synced to Make!")
+            except Exception as e:
+                st.error(
+                    f"An error occurred while creating the DataFrame: {e}")
+                st.write(results)  # Print the data to see its content
+
+
+if __name__ == '__main__':
     multiprocessing.freeze_support()
     main()
