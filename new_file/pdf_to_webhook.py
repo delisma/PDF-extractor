1bce031e353ca22d2251294e04aaac97c2858838
delisma <david.elisma@tbs-sct.gc.ca>
2023-08-03 21:04:27 -0400

    Convert PDF file into images, extract text from images, extract structured information from text using Langchain library, and send extracted data to make.com via a webhook.

--- new_file/pdf_to_webhook.py
+++ new_file/pdf_to_webhook.py
@@ -1,24 +1,38 @@
-import os
-import requests
+from langchain.chat_models import ChatOpenAi
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from pytesseract import image_to_string
 from PIL import Image
-from pdf2image import convert_from_path
-import pytesseract
-from langchain import LangChain
+from io import BytesIO
+import pypdfium2 as pdfium
+import streamlit import st
+import multiprocessing
+from tempfile import NamedTemporaryFile
+import pandas as pd
+import json
+import requests
 
 # Set the OpenAI API key
+
 os.environ["OPENAI_API_KEY"] = 'your-api-key'
 
+# 1 Convert a PDF file into images using PyPDFium.
 def convert_pdf_to_images(pdf_path):
     # TODO: Convert the PDF to images
 
+# 2 Extract text from the images using PyTesseract.
 def extract_text_from_image(image_path):
     # TODO: Extract text from the image
 
+# 3 Extract structured information from the text using the Langchain library to access the OpenAI API.
 def extract_info_from_text(text):
     # TODO: Extract structured information from the text
 
+
+# 4 Send the extracted data to make.com via a webhook.
 def send_data_to_webhook(data):
     # TODO: Send the data to make.com via a webhook
+
 
 def main():
     # TODO: Main function to tie all the steps together
